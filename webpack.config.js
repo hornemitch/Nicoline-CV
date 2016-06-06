c96bbba928b09a09e82be5e6995fd5fda924a596
hornemitch <mitchhorne92@gmail.com>
2016-06-06 21:53:40 +0200

    Initialize commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    entry: "./src/entry.js",
+    output: {
+        path: __dirname,
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+        {   test: /\.less$/,
+            loader: "style!css!less"
+        }
+        ]
+    }
+};
